b358e0ea6282a8b2b3716633e9424d77f37c79dd
elainatesfai <elaina.tesfai@gmail.com>
2022-11-30 13:52:36 +0000

    Some organisational iterations and minor edits
    Added the backend and front-end folders to the workspace. Edited the navbar and home components. Just some minor changes.

---
+++ frontend/src/components/hero/hero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function hero() {
+  return (
+    <div className='HeroContainer'>
+        <img className='HeroBg' src='../assets/images/hero2.png' width='100%' alt=''/>
+    </div>
+  )
+}
+
+export default hero
