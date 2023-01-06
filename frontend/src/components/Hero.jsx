805e2e7c1728710f0e6b3719cc08d731140d644b
elainatesfai <elaina.tesfai@gmail.com>
2023-01-06 00:14:11 +0000

    Added a button to the home page
    Added a button to the homepage, debating whether to change the colour of the navbar in order to attempt to brighten up the website and make it look more engaging for the user.

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -7,11 +7,14 @@ function hero() {
       <div className='HeroContentContainer'>
         <div className='HeroTextContainer'>
           <h1 div className='HeroMainText'>
-            Welcome to Unitriition, fName!
+            Welcome to <span>Unitrition</span>, fName!
           </h1>
           <p className='HeroSubTitle'>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas accusamus tempore temporibus rem amet laudantium animi optio voluptatum. Natus obcaecati unde porro nostrum ipsam itaque impedit incidunt rem quisquam eos!
           </p>
+          <div className='HeroBtnWrapper'>
+            <button>Account Settings</button>
+          </div>
         </div>
         <div className='HeroImgContainer'>
           <img src='./assets/images/Hero.svg' alt='' />
