74407f82819a6e9a56cbcf595697a1aad0a5572b
elainatesfai <elaina.tesfai@gmail.com>
2023-01-07 02:30:04 +0000

    Continued changes on Hero + Navbar
    Added new hero image, changed the app.css root colours and added some more images -> going to continue to work through the variety of issues still remaining in the implementation. Focus soon will be getting the account information from DB to show up once button clicked.

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+
+import { RiUserSettingsLine } from 'react-icons/ri';
+
 import '../css/hero.css'
 
 function hero() {
@@ -7,17 +10,20 @@ function hero() {
       <div className='HeroContentContainer'>
         <div className='HeroTextContainer'>
           <h1 div className='HeroMainText'>
-            Welcome to <span>Unitrition</span>, fName!
+            Welcome to <span>Unitrition</span> fName!
           </h1>
           <p className='HeroSubTitle'>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas accusamus tempore temporibus rem amet laudantium animi optio voluptatum. Natus obcaecati unde porro nostrum ipsam itaque impedit incidunt rem quisquam eos!
           </p>
           <div className='HeroBtnWrapper'>
-            <button>Account Settings</button>
+            <button>
+              Account Settings
+              <RiUserSettingsLine />
+            </button>
           </div>
         </div>
         <div className='HeroImgContainer'>
-          <img src='./assets/images/Hero.svg' alt='' />
+          <img src='./assets/images/HeroV3.svg' alt='' />
         </div>
       </div>
     </div>
