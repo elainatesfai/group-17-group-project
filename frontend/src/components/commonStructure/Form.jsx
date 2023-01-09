c04ff4f737f5606b8f9e48d4650a73bd56811af4
elainatesfai <elaina.tesfai@gmail.com>
2023-01-09 01:35:35 +0000

    User Settings
    Getting started on being able to show the user their settings and retrieving it from the database to display on the modal component -> form is inputted in model.jsx. Yet to code the functions that make that section displayed on the window.

---
+++ frontend/src/components/commonStructure/Form.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../../css/form.css'
+
+function Form() {
+  return (
+    <div className='FormSection'>
+        <div className='FormContainer'>
+        <div className='FormTitle'>
+                <div className='FormRow'>
+                    <div className='FormColumn'>
+                        <h1 className='FormTitle'>
+                            User Settings
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Form
---
+++ frontend/src/components/commonStructure/Modal.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Modal() {
+  return (
+    <div className='ModalBackground'>
+      <div className='ModalWrapper'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Modal
